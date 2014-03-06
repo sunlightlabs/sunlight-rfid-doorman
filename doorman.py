481603a06e1362e7f4e031b4008631b95cf8da48
sbma44 <thomas.j.lee@gmail.com>
2014-03-06 17:15:53 -0500

    now pulling IDs from google spreadsheets at startup

--- doorman.py
+++ doorman.py
@@ -1,21 +1,43 @@
 import time
 import re
+import itertools
 
 import serial
 import gspread
+import envoy
 
 from settings import *
 
 
-def approved_id_list():
-	for x in ['74E81E0587', 'A43D6FBB4D']:
-		yield x
+last_id_list_update = None
+
+def refresh_access_control_list():	
+	gc = gspread.login(USERNAME, PASSWORD)
+	sh = gc.open(SPREADSHEET_NAME)	
+	worksheet = sh.worksheet(SPREADSHEET_WORKSHEET)
+	key_cells = worksheet.col_values(1)
+	email_cells = worksheet.col_values(2)
+	active_cells = worksheet.col_values(3)
+	
+	acl = {}
+
+	for (i, (key, email, active)) in enumerate(izip(key_cells, email_cells, active_cells)):
+		if i==0:
+			continue
+		if active.upper().strip()=='Y':
+			acl[key.strip()] = email.strip()
+
+	last_id_list_update = time.time()
+
+	return acl
 
 def main():
 
+	acl = refresh_access_control_list()
+
 	re_identifiers = re.compile(r'\[(.{10})\]')
 
-	print 'starting...'
+	print 'Starting...'
 
 	# open serial connection
 	ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
@@ -23,16 +45,20 @@ def main():
 	# wait for Arduino to restart due to serial connection
 	time.sleep(2) 
 
-	while True:
-		print 'waiting...'
-		line = ser.readline()
-		print 'got %s' % line
+	print 'Ready.'
+	print ('-' * 60)
+
+	while True:		
+		line = ser.readline()		
 		match = re_identifiers.search(line)
-		if match is not None:
-			print 'found ID %s' % match.group(1)
-			if match.group(1) in approved_id_list():
-				ser.write('G')
+		if match is not None:		
+			serial = match.group(1)	
+			if match.group(1) in acl.keys():
+				ser.write('G')				
+				envoy.run('ssh gatekeeper /usr/bin/python /root/cycle.py')
+				print '%s (%s) - [OK]' % (serial, acl[serial])
 			else:
+				print '%s - [DENY]' % (serial)
 				ser.write('R')
 
 
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ SERIAL_PORT = '/dev/ttyACM0'
 SERIAL_RATE = 115200
 
 SPREADSHEET_NAME = 'Sunlight Door Access'
+SPREADSHEET_WORKSHEET = 'ACL'
 SPREADSHEET_USER = 'judgebrandeis@sunlightfoundation.com'
 SPREADSHEET_PASSWORD = 'set this in local_settings.py'
 
